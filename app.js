6581495da81cf2fb83e190b97c002f2807272af5
SmRon <kovacs.code@gmail.com>
2023-05-14 14:09:48 +0200

    Add Google login and Finishing Up the App

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use(session({
   secret: process.env.SECRET,
@@ -27,40 +28,45 @@ app.use(passport.session());
 
 mongoose.connect(process.env.MONGO_CONNECT);
 
+
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     googleId: String,
+    secret: String,
 });
+
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
-    return cb(null, {
-      id: user.id,
-      username: user.username,
-      picture: user.picture
-    });
-  });
+passport.use(User.createStrategy());
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(user, cb) {
-  process.nextTick(function() {
-    return cb(null, user);
-  });
+passport.deserializeUser(function(id, done) {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
+    });
 });
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://titkos.herokuapp.com/auth/google/secrets",
+    callbackURL: "http://localhost:3000/auth/google/secrets",
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
+
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -72,47 +78,61 @@ passport.use(new GoogleStrategy({
 app.get("/", function(req, res){
   res.render("home");
 });
+
 app.get("/auth/google",
   passport.authenticate("google", { scope: ["profile"]})
 );
+
 app.get("/auth/google/secrets",
   passport.authenticate('google', { failureRedirect: "/login" }),
   function(req, res) {
+    // Successful authentication, redirect to secrets.
     res.redirect("/secrets");
   });
+
 app.get("/login", function(req, res){
   res.render("login");
 });
+
 app.get("/register", function(req, res){
   res.render("register");
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}).exec()
+  .then(foundUsers => {
+    if (foundUsers) {
+      res.render("secrets", {usersWithSecrets: foundUsers});
+    }
+  }).catch(err => {console.log(err);})
+
+});
+app.get("/logout", function(req, res){
+  req.logout(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
 });
-app.get("/logout", function(req, res){
-  req.logout(function(err){
-    if (err) {
-      console.log(err);
-      res.redirect("secrets");
-    } else {
-      res.redirect("/");
-    }
-  });
-});
 
 app.post("/register", function(req, res){
+
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err) {
       console.log(err);
       res.redirect("/register");
     } else {
       passport.authenticate("local")(req, res, function(){
-        res.redirect("/secrets");
+      res.redirect("/secrets");
       });
     };
   });
@@ -127,12 +147,32 @@ app.post("/login", function(req, res){
  req.login(user, function(err){
    if (err) {
      console.log(err);
+     res.redirect("/register");
    } else {
      passport.authenticate("local")(req, res, function(){
        res.redirect("/secrets");
      });
-   };
+   }
  });
+});
+
+app.post("/submit", function(req, res){
+const subbmittedSecret = req.body.secret;
+console.log(req.user);
+User.findById(req.user.id).exec()
+.then(foundUser => {
+  console.log(foundUser);
+  if(foundUser){
+    foundUser.secret = subbmittedSecret;
+    foundUser.save()
+    .then(() => {
+      res.redirect("/secrets");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+}).catch(err => {console.log(err);});
 });
 
 
